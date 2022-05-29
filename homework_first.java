41b37cf72508aeec4e4a9656bfdc1a407a71dc53
PeterSmokery <PeterSmokery@gmail.com>
2022-05-29 19:40:13 +0300

    первый синтаксиз на java

---
+++ homework_first.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class homework_first {
+    public static void main(String[] args) {
+        Scanner iScanner = new Scanner(System.in);
+        System.out.printf("введите целове число которое хотите возвести в степень: ");
+        int a = iScanner.nextInt();
+        System.out.printf("введите целую степень в которую хотите возвести: ");
+        int b = iScanner.nextInt();
+        double res = Math.pow(a, b);
+        File f = new File("output.txt");
+        if (f.exists() && !f.isDirectory()) {
+            System.out.println("файл уже существует.перезаписать?1-да,0-нет");
+            int think = iScanner.nextInt();
+            if (think == 1)
+                try (FileWriter fw = new FileWriter("output.txt", false)) {
+                    if (a == 0 & b == 0)
+                        fw.write("Ответ:не определено");
+                    else
+                        fw.write("Ответ:");
+                    fw.write(Double.toString(res));
+                    fw.flush();
+                } catch (IOException ex) {
+                    System.out.println(ex.getMessage());
+                }
+
+        }
+
+    }
+}
